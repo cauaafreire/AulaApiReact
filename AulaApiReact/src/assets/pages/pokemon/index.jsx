09beffb6a2f2cb3d1c6bdbfc538efeaf8f343cde
Cauã freire <cauafreirw@gmail.com>
2026-04-30 17:00:07 +0000

    Adicionando as paginas

---
+++ AulaApiReact/src/assets/pages/pokemon/index.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+const pokemonList = [
+    {id:1, nome:"Bulbassauro"},
+    {id:4, nome:"Charmander"},
+    {id:7, nome:"Squirtle"},
+    {id:6, nome:"Charizard"},
+    {id:152, nome:"Chikorita"}
+]
+
+function Pokemon() {
+    return (
+        <div>
+            <h1>Lista de Pokémon</h1>
+        </div>
+    )
+}
+
+export default Pokemon
